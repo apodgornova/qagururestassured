121d69c9b7486efc5e3915dc9ffdf91a6abed8d1
a.podgornova <a.podgornova@robo.finance>
2022-01-02 14:53:13 +0700

    DemowebshopTests tests

---
+++ src/test/java/DemowebshopTests.java
@@ -0,0 +1,80 @@
+import com.codeborne.selenide.Configuration;
+import io.restassured.RestAssured;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.Cookie;
+
+import static com.codeborne.selenide.Condition.attribute;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+import static com.codeborne.selenide.WebDriverRunner.getWebDriver;
+import static io.qameta.allure.Allure.step;
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.core.Is.is;
+
+public class DemowebshopTests {
+
+    @BeforeAll
+    static void setUp() {
+        RestAssured.baseURI = "http://demowebshop.tricentis.com";
+        Configuration.baseUrl = "http://demowebshop.tricentis.com";
+    }
+
+    @Test
+    void okAuthUserTest() {
+        step("Get cookie by api and set it to browser", () -> {
+            String authorizationCookie =
+                    given()
+                            .contentType("application/x-www-form-urlencoded; charset=UTF-8")
+                            .formParam("Email", "nastya1@testemail.com")
+                            .formParam("Password", "Testpass123")
+                            .when()
+                            .post("/login")
+                            .then()
+                            .statusCode(302)
+                            .extract()
+                            .cookie("NOPCOMMERCE.AUTH");
+
+            step("Open minimal content, because cookie can be set when site is opened", () ->
+                    open("/Themes/DefaultClean/Content/images/logo.png"));
+
+            step("Set cookie to to browser", () ->
+                    getWebDriver().manage().addCookie(
+                            new Cookie("NOPCOMMERCE.AUTH", authorizationCookie)));
+        });
+
+        step("Open main page", () ->
+                open(""));
+
+        step("Open profile page", () ->
+                open("/customer/info"));
+
+        step("Verify First name is proper on profile page", () ->
+                $("#FirstName").shouldHave(attribute("value", "Anastasia"))
+        );
+
+        step("Verify Last name is proper on profile page", () ->
+                $("#LastName").shouldHave(attribute("value", "Podgornova"))
+        );
+
+    }
+
+    @Test
+    void addToWishlist() {
+
+        String body = "addtocart_51.EnteredQuantity=1";
+
+        step("Add product to wishlist", () -> {
+            given()
+                    .contentType("application/x-www-form-urlencoded; charset=UTF-8")
+                    .body(body)
+                    .when()
+                    .post("addproducttocart/details/51/2")
+                    .then()
+                    .statusCode(200)
+                    .body("updatetopwishlistsectionhtml", is("(1)"))
+                    .body("message", is("The product has been added to your <a href=\"/wishlist\">wishlist</a>"));
+        });
+    }
+
+}
